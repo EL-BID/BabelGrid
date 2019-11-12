0709239d9d455738323cbe2cf760b24295d8ed02
Joaoc <joaom@iadb.org>
2019-11-12 16:14:28 -0500

    Ãadd basic tests

--- tests/test_s2.py
+++ tests/test_s2.py
@@ -18,5 +18,29 @@ class Tests2(unittest.TestCase):
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
-    def test_000_something(self):
+    def test_geo_to_s2(self):
         """Test something."""
+
+        lat, lon, res = 12, -3, 10
+        s2_address = s2.geo_to_s2(lat, lon, res)
+
+        assert isinstance(s2_address, str)
+        assert s2_address == '0e3229'
+
+    def test_s2_to_geo_boundary(self):
+
+        s2_address = '0e3229'
+
+        geo_boundary = [[11.96432861265856, -3.017571181608344],
+                        [11.965172505621743, -2.9375738929377695],
+                        [12.056429667269397, -2.9375738929377695],
+                        [12.055579720951927, -3.017571181608344]]
+
+        geo_boundary_geojson = [[-3.017571181608344, 11.96432861265856], 
+                                [-2.9375738929377695, 11.965172505621743], 
+                                [-2.9375738929377695, 12.056429667269397], 
+                                [-3.017571181608344, 12.055579720951927], 
+                                [-3.017571181608344, 11.96432861265856]]
+
+        assert s2.s2_to_geo_boundary(s2_address) == geo_boundary
+        assert s2.s2_to_geo_boundary(s2_address, True) == geo_boundary_geojson
